74b468db996030a7ac663af0b6707e5c87d316e0
brian <devnull@localhost>
2001-06-01 12:41:09 -0700

    sanitized supermail.pl

--- superemail.pl
+++ superemail.pl
@@ -1,17 +1,11 @@
 #!/usr/bin/perl -w
 
-# script to page me with an attacker's info
 
-# are we debugging?
-$DEBUG=0; # 1=true, 0=false
-$PAGE_EMAIL="steve\@91x.com";
-#$PAGE_EMAIL="brian\@sunset-cliffs.org";
+$EMAIL="user\@host.com";
 
-#print "Paging $PAGE_EMAIL\n";
-
-$message = "Hi Steve!  My name is Brian, and my phone number is 619-226-6003";
+$message = "This is where your message should go";
 
 for ($x=0; $x <= 50; $x++) {
-	system("echo $message | /usr/bin/mail -s \"DM\" $PAGE_EMAIL");
+	system("echo $message | /usr/bin/mail -s \"subject\" $EMAIL");
 }
 
