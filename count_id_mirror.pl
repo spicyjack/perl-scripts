ecb7e8de6b689e6cf315b349af69054f049b60ce
brian <devnull@localhost>
2001-10-31 17:36:07 -0800

    - count_id_mirror works, the doom archive is 3.3G right now

--- count_id_mirror.pl
+++ count_id_mirror.pl
@@ -9,19 +9,28 @@
 if (! $ARGV[0]) { print "Usage: $0 ls_output\nExiting...\n"; exit 1;}
 
 # open the file for reading
+print "$0: opening $ARGV[0]\n";
 $ls_output = open(LSOUT, $ARGV[0]);
 
 # a counter would be nice
 $counter = 0;
+#$tmpcounter = 0;
 
 # loop thru the file
 while (<LSOUT>) {
-	
 	# any line without a d or '-' as the first character, discard
-	if ( /^-/ || /^d/ ) { # we've got a match
+	#if ( /^-/ || /^d/ ) { # we've got a match
+	if ( /^-/ ) { # we don't download directories, so don't count them
+		# we've got a match
 		# we want the 4th column from 0 (left edge)
-		@line = split ($_, " ");	
+		# /\s+/ == one or more spaces
+		chomp($_);
+		@line = split (/\s+/, $_);	
+		#print join(":", @line);
+		#print "\n";
 		$counter += $line[4];
+		#$tmpcounter +=1;
+		#if ($tmpcounter == 10) {print "Tempcounter limit: $counter\n"; exit 1;}
 	} # if ( /^-/ || /^d/ )
 } # while (<LSOUT>)
 
