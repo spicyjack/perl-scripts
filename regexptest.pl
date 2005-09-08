a46cb65803bea1dd746cff96b87522e690d9476c
brian <devnull@localhost>
2005-09-08 12:06:07 -0700

    - playing with a pattern that pulls extensions from filenames

--- regexptest.pl
+++ regexptest.pl
@@ -4,7 +4,7 @@
 
 print "please input a test string: ";
 $input = <STDIN>;
-$input =~ s/^\w+\.//g;
-print "substituted output is $input\n";
+$input =~ s/(\w+\.)//g;
+print "substituted output is: >$input<\n";
 print "Thanks for playing!!\n";
 
