316248ff4789adcf3f94461fbd5a43e180a11fd9
brian <devnull@localhost>
2005-12-29 16:33:07 -0800

    - new regex for dequoting

--- regexptest.pl
+++ regexptest.pl
@@ -5,7 +5,8 @@
 print "please input a test string: ";
 $input = <STDIN>;
 chomp($input);
-$input =~ s/^(\d+).*\.mp3/$1/g;
+$input =~ s/^"(.*)"$/$1/g;
+$input =~ s/""/"/g;
 print "substituted output is: >$input<\n";
 print "Thanks for playing!!\n";
 
