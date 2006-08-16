8a9bc03909c98b1f462c3e321baf591b79cf9f5c
brian <devnull@localhost>
2006-08-16 14:07:13 -0700

    - changed the regexptest to save the first match and output

--- regexptest.pl
+++ regexptest.pl
@@ -5,7 +5,7 @@
 print "please input a test string: ";
 $input = <STDIN>;
 chomp($input);
-$input =~ s/.*\.(\w+)$/$1/;
+$input =~ /^Package: (\w+)$/;
 print "substituted output is: >$input<\n";
 print "first variable is >$1<\n";
 print "Thanks for playing!!\n";
