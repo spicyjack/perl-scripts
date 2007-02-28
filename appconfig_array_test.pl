053198cea4d559fd103bcab6cb25b026fb5919a2
brian <devnull@localhost>
2007-02-28 00:47:09 -0800

    - added more demos to the appconfig demo script

--- appconfig_array_test.pl
+++ appconfig_array_test.pl
@@ -14,6 +14,21 @@ $Config->set(q(libpath), q(/usr/lib/perl5));
 # array AppConfig objects.  Poopy.
 
 $Config->args(\@ARGV);
+&printit($Config);
 
-print qq(The joined libpath is:\n) 
-    . join(q(:), @{$Config->get(q(libpath))} ) . qq(\n);
+# this doesn't work either, each successive set() method call adds the value to
+# the array, no matter what; there's no way as far as I know to 'unset' a
+# variable
+$Config->set(q(libpath), undef);
+&printit($Config);
+
+# this works however...
+$Config->_default(q(libpath));
+
+&printit($Config);
+
+sub printit {
+    my $Config = shift;
+    print qq(The joined libpath is:\n) 
+        . join(q(:), @{$Config->get(q(libpath))} ) . qq(\n);
+} # sub printit
