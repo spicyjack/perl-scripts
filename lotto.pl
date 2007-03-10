3ce8dbe1176abb9d6623f64d8cf4f15aa117233f
brian <devnull@localhost>
2007-03-09 16:35:52 -0800

    - added some URLs to lotto numbers

---
+++ lotto.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+# script to parse lottery winning numbers files in order to show the numbers
+# with the highest frequency
+
+# winning lottery numbers:
+# http://tinyurl.com/27gnzm (MegaMillions)
+# http://tinyurl.com/yv3xd8 (SuperLotto)
+#
+# CNN/Money article on a lottery winner's investments
+# http://tinyurl.com/337nyj
