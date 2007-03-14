96593fcb67a7b23ec887e6a04ace9ed705e34347
brian <devnull@localhost>
2007-03-13 18:06:41 -0700

    - added sample data from the tasklist, so that I can convert the tasklist into   something 'smarter'

--- yaml-test.pl
+++ yaml-test.pl
@@ -34,10 +34,51 @@ print Dumper $hash;
 # end of the script
 1;
 
-__DATA__
----
+=pod
+
 root_device: /dev/sda2
 # use the X's below as a placeholder for the key ID in your disk key filename
 disk_encryption_key_file: user_disk_key-XXXXXXXX.gpg
 max_user_keys: 40
+
+
+--- 
+- MTL/MSG Perl Testing Modules Survey
+  percent_complete: 100
+  task_duration: 8
+  task_description: >
+    Survey the MTL/MSG Perl modules, with an eye towards answering the
+    following questions:
+      - When the Perl framework acts as a client, what messages are being sent,
+        how often are they being sent, and from which modules are they being
+        sent
+      - How does the Perl MTL framework keep track of connections from the
+        binary components?
+      - If the SourceIdentification message went away, would the Perl framework
+        still function correctly?
+
+ - 
+ 
+
+
+=cut
+
+__DATA__
+#%YAML 1.1
+---
+3.0-xcast/3.L2 Perl Testing Framework Integration:
+ - percent_complete: 100
+   task_duration: 8
+   task_description: >
+     Integrate 3.0-xcast and 3.L2 testing/perl directories in Perforce
+ - percent_complete: 100
+   task_duration: 8
+   task_description: >
+     Integrate 3.0-xcast and 3.L2 DIST/OSS Perl testing frameworks directories
+     in Perforce
+ - percent_complete: 100
+   task_duration: 8
+   task_description: >
+     Run DIST/OSS sample tests using either branch of the testing frameworks
+     in Perforce
 ...
