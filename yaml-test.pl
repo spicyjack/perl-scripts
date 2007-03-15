edabec2f1c0b7a5a4fd8544df7c38dea90c03be7
brian <devnull@localhost>
2007-03-14 17:34:33 -0700

    - added more complex YAML data

--- yaml-test.pl
+++ yaml-test.pl
@@ -36,30 +36,11 @@ print Dumper $hash;
 
 =pod
 
+# old YAML data
 root_device: /dev/sda2
 # use the X's below as a placeholder for the key ID in your disk key filename
 disk_encryption_key_file: user_disk_key-XXXXXXXX.gpg
 max_user_keys: 40
-
-
---- 
-- MTL/MSG Perl Testing Modules Survey
-  percent_complete: 100
-  task_duration: 8
-  task_description: >
-    Survey the MTL/MSG Perl modules, with an eye towards answering the
-    following questions:
-      - When the Perl framework acts as a client, what messages are being sent,
-        how often are they being sent, and from which modules are they being
-        sent
-      - How does the Perl MTL framework keep track of connections from the
-        binary components?
-      - If the SourceIdentification message went away, would the Perl framework
-        still function correctly?
-
- - 
- 
-
 
 =cut
 
@@ -69,16 +50,33 @@ __DATA__
 3.0-xcast/3.L2 Perl Testing Framework Integration:
  - percent_complete: 100
    task_duration: 8
+   task_title: 3.0-xcast and 3.L2 Integration, Part 1
    task_description: >
      Integrate 3.0-xcast and 3.L2 testing/perl directories in Perforce
  - percent_complete: 100
    task_duration: 8
+   task_title: 3.0-xcast and 3.L2 Integration, Part 2
    task_description: >
      Integrate 3.0-xcast and 3.L2 DIST/OSS Perl testing frameworks directories
      in Perforce
  - percent_complete: 100
    task_duration: 8
+   task_title: Run DIST/OSS tests
    task_description: >
      Run DIST/OSS sample tests using either branch of the testing frameworks
      in Perforce
+MTL/MSG Perl Testing Modules Survey:
+ - percent_complete: 100
+   task_duration: 8
+   task_title: MTL/MSG Perl Testing Modules Survey
+   task_description: >
+     Survey the MTL/MSG Perl modules, with an eye towards answering the
+     following questions:
+      - When the Perl framework acts as a client, what messages are being sent,
+        how often are they being sent, and from which modules are they being
+        sent
+      - How does the Perl MTL framework keep track of connections from the
+        binary components?
+      - If the SourceIdentification message went away, would the Perl framework
+        still function correctly?
 ...
