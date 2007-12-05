d2b526da1763a39a5f578963109c03402454150b
brian <devnull@localhost>
2007-12-04 18:44:52 -0800

    - added stub for Net::SNMP demo - added glade sample files

---
+++ netsnmp_demo.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+# $Id$
+# Copyright (c)2001 by Brian Manning
+#
+# demo of Net::SNMP
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; version 2 dated June, 1991.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program;  if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111, USA.
+
+
+# vi: set ft=perl sw=4 ts=4 cin:
+# end of line
+1;
+
