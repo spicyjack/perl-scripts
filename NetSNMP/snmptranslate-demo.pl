94763e501bf3d5dbc458fcd6ba051000435eb1eb
brian <devnull@localhost>
2008-01-03 18:08:16 -0800

    - added demo script for SNMP::Translate

---
+++ NetSNMP/snmptranslate-demo.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+# $Id$
+# borrowed from the Net::SNMP Perl POD page
+#
+# demo of SNMP::Translate
+
+# external modules
+use lib qw(../../SNMP-Translate/lib);
+use SNMP::Translate;
+
+# create an SNMP session
+my $rosetta = SNMP::Translate->new( oid => q(.1.3.6.1.2.1.1.3) );
+
+$rosetta->translate();
+
+exit 0;
+
+# vi: set ft=perl sw=4 ts=4 cin:
+# EOL
