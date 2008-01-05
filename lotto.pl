a043230373ecba8e763adcb580dbb569a1c39180
brian <devnull@localhost>
2008-01-05 02:06:33 -0800

    - added Text::CSV to lotto script

--- lotto.pl
+++ lotto.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl
 
+# $Id$
+# Copyright (c)2007 by Brian Manning <elspicyjack at gmail dot com>
+
 # script to parse lottery winning numbers files in order to show the numbers
 # with the highest frequency
 
@@ -9,3 +12,26 @@
 #
 # CNN/Money article on a lottery winner's investments
 # http://tinyurl.com/337nyj
+
+use strict;
+use warnings;
+
+# external modules
+use Text::CSV;
+
+my $csv = Text::CSV->new( {
+        sep_char        => ' ',
+        eol             => qq(\n)
+});
+
+open(CSV,"<DownloadAllNumbers.htm");
+my $counter = 0;
+foreach my $line (<CSV>) {
+    # munge out extra spaces
+    chomp($line);
+    $line =~ s/\s+/ /g; 
+    my $status = $csv->parse($line);
+    print qq(Columns are: ) . join(q(:), $csv->fields() ) . qq(\n);
+    $counter++;
+    if ( $counter == 10 ) { exit 0; }
+} # foreach my $line (<CSV>)
