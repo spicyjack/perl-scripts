61810794435147cc52eb40c57468b660b9aff39e
brian <devnull@localhost>
2008-03-18 18:23:43 -0700

    - added more guts to the Moose classes

--- archive_diff.pl
+++ archive_diff.pl
@@ -27,7 +27,8 @@
 
 =head1 NAME
 
-archive_diff.pl
+B<archive_diff.pl> compares the contents of two archives, checking for files
+that are in one archive and not the other and vice versa.
 
 =head1 VERSION
 
@@ -36,8 +37,7 @@ the author's version number.
 
 =head1 SYNOPSIS
 
-B<archive_diff.pl> compares the contents of two archives, checking for files
-that are in one archive and not the other and vice versa.
+ perl archive_diff.pl -1 first_filelist.txt -2 second_filelist.txt 
 
 =cut
 
@@ -51,8 +51,9 @@ use Log::Log4perl qw(get_logger :levels);
 use Time::Local;
 use Pod::Usage;
 
-my ($DEBUG, $first_file, $second_file, $first_obj, $second_obj);
-my $colorlog = 1;
+my ($DEBUG, $first_file, $second_file, $first_obj, $second_obj, $colorlog);
+# colorize Log4perl output by default 
+$colorlog = 1;
 
 my $goparse = Getopt::Long::Parser->new();
 $goparse->getoptions(   q(DEBUG|D)                      => \$DEBUG,
@@ -61,6 +62,9 @@ $goparse->getoptions(   q(DEBUG|D)                      => \$DEBUG,
                         q(second-file|second|2nd|2=s)   => \$second_file,
                         q(colorlog!)                    => \$colorlog,
                     ); # $goparse->getoptions
+
+# always turn off color logs under Windows, the terms don't do ANSI
+if ( $^O =~ /MSWin32/ ) { $colorlog = 0; } 
 
 # set up the logger
 my $logger_conf = qq(log4perl.rootLogger = INFO, Screen\n);
@@ -104,9 +108,12 @@ if ( defined $second_file ) {
     &HelpDie;
 } # if ( defined $first_file ) 
 
-my $diff = Archive::Diff->new();
+# print some debugging
+print qq(The first object's filename is ) . $first_obj->get_filename();
+print qq(The second object's filename is ) . $second_obj->get_filename();
+my $diff = Archive::Diff->new( first => $first_obj, second => $second_obj );
 
-#$diff->compare(first => $first_file, second => $second_file);
+$diff->simple_diff();
 
 exit 0;
 
@@ -157,7 +164,7 @@ readable when an object is created from this class.
 
 A hash of L<Archive::File> objects keyed by filename.
 
-=back 5
+=back
 
 =cut
 
@@ -177,6 +184,17 @@ has q(attrib) => ( is => q(rw), isa => q(Archive::Attributes));
 has q(filename) => ( is => q(rw), isa => q(ArchiveFilename), required => 1 );
 has q(contents) => ( is => q(rw), isa => q(HashRef[Archive::File]) );
 
+=pod
+
+=head3 get_filename()
+
+Returns the filename of the archive
+
+=cut 
+
+sub get_filename { return shift->filename; }
+
+#### end Package 'Archive' ####
 
 =pod
 
@@ -185,6 +203,19 @@ has q(contents) => ( is => q(rw), isa => q(HashRef[Archive::File]) );
 This file takes two L<Archive> objects as arguments, and when queried using
 the object methods, will return a formatted list that shows the differences in
 the contents of the two archives.
+
+
+=cut
+
+#### Package 'Archive::Diff' ####
+package Archive::Diff;
+use Moose; # comes with 'strict' and 'warnings'
+use Moose::Util::TypeConstraints;
+
+has q(first) => ( is => q(rw), isa => q(Archive), required => 1 );
+has q(second) => ( is => q(rw), isa => q(Archive), required => 1 );
+
+=pod
 
 =head3 simple_diff()
 
@@ -198,15 +229,16 @@ file in both archives differs in timestamp or uncompressed size.
 
 =cut
 
-#### Package 'Archive::Diff' ####
-package Archive::Diff;
+sub simple_diff {
+	my $self = shift;
+	my $first = $self->first;
+	my $second = $self->second;
 
-sub new {
-    print qq(This is Archive::Diff->new\n);
-    my $archive = Archive->new();
-} # sub new
+	print qq(The first filename is ) . $first->filename . qq(\n);
+	print qq(The second filename is ) . $second->filename . qq(\n);
+} # sub simple_diff
 
-#### end Package 'Archive::Attributes' ####
+#### end Package 'Archive::Diff' ####
 
 =pod 
 
@@ -222,7 +254,7 @@ The version of the program that created this archive.  May or may not come from
 the archive itself;  a asterisk B<*> denotes the version number of the program
 on the system, as the archive itѕelf does not hold any version information.
 
-=back 5
+=back
 
 =cut
 
@@ -241,10 +273,7 @@ has q(version) => ( is => q(rw), isa => q(Str) );
 
 An object that holds information about a file in an archive.  You would most
 likely create as many L<Archive::File> objects as you had individual files and
-directories in an archive.  L<Archive::File> can be queried using the
-following methods:
-
-=head3 Attributes 
+directories in an archive.  L<Archive::File> has the following attributes:
 
 =over 5 
 
@@ -264,7 +293,7 @@ The uncompressed size of the file stored in the archive.
 
 The attributes stored with the file in the archive.
 
-=back 5
+=back
 
 =cut
 
@@ -289,5 +318,3 @@ Brian Manning E<lt>elspicyjack at gmail dot comE<gt>
 
 # vi: set sw=4 ts=4 cin:
 # end of line
-1;
-
