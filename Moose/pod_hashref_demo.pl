7ab7c21a0f9fba6cd2703a60746f243185b45ab6
brian <devnull@localhost>
2008-04-30 19:11:27 -0700

    - added demo from HashRef.pod file as a separate file so it can be tested

---
+++ Moose/pod_hashref_demo.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+
+
+    package HashDemo;
+    use Moose;
+    use Moose::Util::TypeConstraints;
+
+    # this attribute is a HashRef
+    has 'files' => ( is => 'rw', isa => q(HashRef) );
+
+    sub dump {
+        my $self = shift; 
+		# de-reference the HashRef stored in $self->files
+		# and enumerate over it's keys
+        foreach my $key ( keys(%{$self->files}) ) {
+			# note that the HashRef contains another hash
+			# hence the $object->{hash1}{hash2} syntax below
+            print qq(this hash key/value pair is $key : )
+                . $self->{files}{$key} . qq(\n);
+        } # foreach my $key
+    } # sub dump
+	
+    package main;
+    use Moose;
+
+    my $demo = HashDemo->new( files => { 1 => q(/path/to/some/file) } );
+	print qq(First dump:\n);
+    $demo->dump;
+    # prints "My hash is 1:/path/to/some/file"
+
+    # this replaces the existing HashRef contents
+    $demo->files( { 2 => q(/path/to/file2), 3 => q(/path/to/file3) } );
+	print qq(Second dump:\n);
+    $demo->dump;
+
+    # this clears the HashRef
+    $demo->files( { } );
+	print qq(Third dump:\n);
+    $demo->dump;
+
+    exit 0;
+
