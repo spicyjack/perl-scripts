4c23d3d933d7a1e8ad35bfc9b9fa5e4011568e5b
elspicyjack <elspicyjack at gmail dot com>
2009-09-13 23:56:35 -0700

    Added cart_to_radians demo; sincos_cartesian displays correct coordinates now

--- cart_to_radians.pl
+++ cart_to_radians.pl
@@ -9,10 +9,14 @@ use Math::Trig;
 my $start_x = 234;
 my $start_y = -178;
 
-#my $rho = sqrt( ($start_x^2) + ($start_y^2) );
-my $rho = $start_x;
-print qq(rho is $rho\n);
-my @angles = ( 320, 325, 330, 335, 340, 345, 350, 355, 360 );
+my $rho = sqrt( ($start_x**2) + ($start_y**2) );
+print qq( x squared = ) . ($start_x**2) . qq(\n);
+print qq( y squared = ) . ($start_y**2) . qq(\n);
+#my $rho = $start_x;
+print qq(starting rho is $rho\n);
+my $current_theta = 360 + rad2deg(atan2($start_y, $start_x));
+print qq(starting theta is $current_theta\n);
+my @angles = ( 320, 322, 323, 325, 330, 335, 340, 345, 350, 355, 360 );
 
 print qq(starting X and Y are $start_x, $start_y\n);
 foreach my $theta ( @angles ) {
