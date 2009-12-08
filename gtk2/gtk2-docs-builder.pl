ae994271283d1d8362e8b7c0f0d8ad662823c0d7
elspicyjack <elspicyjack at gmail dot com>
2009-12-08 10:04:05 -0800

    added dpkg command to list installed Gtk2-Perl modules

--- gtk2/gtk2-docs-builder.pl
+++ gtk2/gtk2-docs-builder.pl
@@ -3,5 +3,10 @@
 # script to automatically build the Gtk2-Perl modules, so that the
 # documentation can be extracted from them and then distributed
 
+# grab the list of git repos to sync with
 #  lynx -dump http://git.gnome.org/cgit/ | grep "\/perl-" | grep  "\. http" |
 #  awk '{ print $2}' | uniq
+
+# get a list of currently installed Gtk2-Perl packages on a Debian machine
+# dpkg -l | egrep "gtk2|glib|pango|cairo|gnome2" | grep perl | awk '{print $3,
+# $2}'
