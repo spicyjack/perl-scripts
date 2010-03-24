bd85efe55f1f00d4bd3abe7c7ba05dd55ac70775
elspicyjack <elspicyjack at gmail dot com>
2010-03-24 00:43:26 -0700

    added a script to test working with UTF-8 text

---
+++ i18n-tests/random_words.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+
+# random_words.pl
+
+# read a text file full of words and their definitions, separated by a comma,
+# then choose a random number of words based on the user's input
+
+use strict;
+use warnings;
+use Getopt::Long;
+use IO::File;
+
+    my ($input_file, $num_of_words);
+    my $parser = Getopt::Long::Parser->new();
+    $parser->getoptions(
+        q(input|i=s) => \$input_file,
+        q(number|n=i) => \$num_of_words,
+    ); # $parser->getoptions
+    
+    my $INFD;
+    if ( defined $input_file ) {
+        $INFD = IO::File->new(qq( < $input_file));
+    } else {
+        $INFD = IO::Handle->new_from_fd(fileno(STDIN), q(<));
+    } # if ( length($parsed{input}) > 0 )
+    my @lines=<$INFD>;
+    my $counter = 0;
+
+    foreach my $line (@lines) {
+        chomp($line);
+        print $line . qq(\n);
+        #print $line . q( ) . sprintf(q(%0x), $line) . qq(\n);
+    }
+
