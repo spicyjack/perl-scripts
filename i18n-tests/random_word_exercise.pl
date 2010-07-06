5bf08988417bbf08ad5235878ca6ce6888ff6f23
Brian Manning <elspicyjack at gmail dot com>
2010-07-06 09:19:36 -0700

    added more data to the random words exercise

--- i18n-tests/random_word_exercise.pl
+++ i18n-tests/random_word_exercise.pl
@@ -29,29 +29,11 @@ END_PERSON
         понравилось
         понравились
 END_VERB
-    my 
+
     binmode STDOUT, ":utf8";
-    if ( defined $input_file ) {
-        $INFD = IO::File->new(qq( < $input_file));
-    } else {
-        $INFD = IO::Handle->new_from_fd(fileno(STDIN), q(<));
-    } # if ( length($parsed{input}) > 0 )
-    my @lines=<$INFD>;
-    my $counter = 0;
 
-    foreach my $line (@lines) {
-        # skip commented lines
-        next if ( $line =~ /^#/ );
-        chomp($line);
-        #print $line . qq(\n);
-        my $decoded_line = decode_utf8($line);
-        my @word = split(//, $decoded_line);
-        my $hexword = q();
-        foreach my $letter ( @word ) {
-            $hexword .= sprintf(q(0x%0x), ord($letter)) . q( );
-        } # foreach my $letter ( @word )
-        # trim any trailing spaces
-        $hexword =~ s/\s+$//;
-        print qq|$decoded_line ($hexword)\n|;
-    } # foreach my $line (@lines)
-
+    # - get a count of how many members each structure has
+    # - get a random number between 0 and the number of members in the
+    # structure
+    # - grab the string that matches the random number
+    # - print it out
