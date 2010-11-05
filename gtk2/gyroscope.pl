463dfb0850bdcc9ce44fa2ef06d3221e2e7eeb6e
Brian Manning <brian at portaboom dot com>
2010-11-05 01:56:01 -0700

    massively cleaned up whitespace

--- gtk2/gyroscope.pl
+++ gtk2/gyroscope.pl
@@ -3,11 +3,11 @@
 # Color Selector
 #
 # $Id$
-# 
+#
 # Original script by Gtk2-Perl team
 #
 # modifications by Brian Manning (elspicyjack {at} gmail &sdot; com)
-# 
+#
 # GtkColorSelection lets the user choose a color. GtkColorSelectionDialog is
 # a prebuilt dialog containing a GtkColorSelection.
 
@@ -23,106 +23,90 @@ my $label;
 my $color;
 
 sub change_color_callback {
-  	my $button = shift;
-  
-  	my $dialog = Gtk2::ColorSelectionDialog->new ("Change color");
+    my $button = shift;
+    my $dialog = Gtk2::ColorSelectionDialog->new ("Change color");
 
-  	$dialog->set_transient_for ($window);
-  
-  	my $colorsel = $dialog->colorsel;
+    $dialog->set_transient_for ($window);
+    my $colorsel = $dialog->colorsel;
 
-  	$colorsel->set_previous_color ($color);
-  	$colorsel->set_current_color ($color);
-  	$colorsel->set_has_palette (TRUE);
-  
-  	my $response = $dialog->run;
+    $colorsel->set_previous_color ($color);
+    $colorsel->set_current_color ($color);
+    $colorsel->set_has_palette (TRUE);
 
-  	if ($response eq 'ok') {
-      	$color = $colorsel->get_current_color;
-      	$da->modify_bg ('normal', $color);
+    my $response = $dialog->run;
 
-		#$label->modify_bg('normal', $color);
-
-	   	print STDERR "Changing color to #" . &stringify_color($color) . "\n";
-	
-		$label->set_text("#" . &stringify_color($color));
-  	}
-
-
-	$dialog->destroy;
+    if ($response eq 'ok') {
+        $color = $colorsel->get_current_color;
+        $da->modify_bg ('normal', $color);
+        #$label->modify_bg('normal', $color);
+        print STDERR "Changing color to #" . &stringify_color($color) . "\n";
+        $label->set_text("#" . &stringify_color($color));
+    }
+    $dialog->destroy;
 }
 
 sub stringify_color {
-	$color = shift;
+    $color = shift;
 
-	return sprintf('%02x', int($color->red / 257) )
-		. sprintf('%02x', int($color->green / 257) )
-		. sprintf('%02x', int($color->blue / 257) ); 
-
+    return sprintf('%02x', int($color->red / 257) )
+        . sprintf('%02x', int($color->green / 257) )
+        . sprintf('%02x', int($color->blue / 257) );
 } # sub stringify_color
 
 ### main script ###
+    # create the window
+    $window = Gtk2::Window->new;
 
+    $color = Gtk2::Gdk::Color->new (0, 65535, 0);
 
-
-	# create the window
-	$window = Gtk2::Window->new;
-
-	$color = Gtk2::Gdk::Color->new (0, 65535, 0);
-
-	$window->set_title ("Color Selection");
-
-	$window->signal_connect (destroy => sub { Gtk2->main_quit; 1 });
-
-	$window->set_border_width (8);
+    $window->set_title ("Color Selection");
+    $window->signal_connect (destroy => sub { Gtk2->main_quit; 1 });
+    $window->set_border_width (8);
 
     my $vbox = Gtk2::VBox->new (FALSE, 8);
-	$vbox->set_border_width (8);
+    $vbox->set_border_width (8);
     $window->add ($vbox);
 
-	#
+    #
     # Create the color swatch area
-	#
-      
+    #
+
     my $colorframe = Gtk2::Frame->new;
-	$colorframe->set_shadow_type ('in');
+    $colorframe->set_shadow_type ('in');
     $vbox->pack_start ($colorframe, TRUE, TRUE, 0);
-    
-	$da = Gtk2::DrawingArea->new;
+
+    $da = Gtk2::DrawingArea->new;
     # set a minimum size
-	$da->set_size_request (200, 100);
+    $da->set_size_request (200, 100);
     # set the color
-	$da->modify_bg ('normal', $color);
-     
+    $da->modify_bg ('normal', $color);
+
     $colorframe->add ($da);
 
-	my $labelframe = Gtk2::Frame->new;
-	$vbox->pack_start ($labelframe, TRUE, TRUE, 0);
+    my $labelframe = Gtk2::Frame->new;
+    $vbox->pack_start ($labelframe, TRUE, TRUE, 0);
 
-	$label = Gtk2::Entry->new();
-	$label->set_text("#" . &stringify_color($color));
-	#$label = Gtk2::Label->new_with_mnemonic("#" . &stringify_color($color));
+    $label = Gtk2::Entry->new();
+    $label->set_text("#" . &stringify_color($color));
+    #$label = Gtk2::Label->new_with_mnemonic("#" . &stringify_color($color));
 
-	$labelframe->add ($label);
+    $labelframe->add ($label);
 
-	my $alignment = Gtk2::Alignment->new (1.0, 0.5, 0.0, 0.0);
-      
+    my $alignment = Gtk2::Alignment->new (1.0, 0.5, 0.0, 0.0);
+
     my $button = Gtk2::Button->new_with_mnemonic ("_Change the above color");
-	$alignment->add ($button);
-      
+    $alignment->add ($button);
     $vbox->pack_start ($alignment, FALSE, FALSE, 0);
-      
-	$button->signal_connect (clicked => \&change_color_callback);
+    $button->signal_connect (clicked => \&change_color_callback);
+    if (!$window->visible) {
+        $window->show_all;
+    } else {
+        $window->destroy;
+        $window = undef;
+    } # if (!$window->visible)
 
-	if (!$window->visible) {
-		$window->show_all;
-	} else {
-		$window->destroy;
-		$window = undef;
-	} # if (!$window->visible)
-
-	# pass control to GTK
-	Gtk2->main;
+    # pass control to GTK
+    Gtk2->main;
 
 1;
 __END__
