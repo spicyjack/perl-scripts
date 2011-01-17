e2ca4d209f87f54ff09edfd60d4c1423910bb472
Brian Manning <brian at portaboom dot com>
2011-01-17 12:45:52 -0800

    fixed whitespace issues, added an LDAP record dump, commented out

--- test_ldap_search.pl
+++ test_ldap_search.pl
@@ -6,9 +6,11 @@ use warnings;
 use Net::LDAP;
 use Term::ReadKey;
 
-my $username = q(bogususer);
+#my $username = q(bogususer);
+my $username = q(bmanning);
 #my $ldap = Net::LDAP->new ( "na.qualcomm.com", debug => 3 ) or die "$@";
 my $ldap = Net::LDAP->new ( "na.qualcomm.com" ) or die "$@";
+#my $ldap = Net::LDAP->new ( "directory.qualcomm.com" ) or die "$@";
 ReadMode("noecho");
 print "Enter the LDAP password: ";
 my $passwd = ReadLine(0);
@@ -16,10 +18,10 @@ ReadMode("restore");
 chomp $passwd;
 print qq(\n);
 
-my $mesg = $ldap->bind ( 
-    "cn=$username,ou=Users,ou=San Diego,dc=na,dc=qualcomm,dc=com",           
+my $mesg = $ldap->bind (
+    "cn=$username,ou=Users,ou=San Diego,dc=na,dc=qualcomm,dc=com",
     password => "$passwd",
-    version => 3 
+    version => 3
 ); # use version 3 for changes/edits
 
 if ( defined $mesg ) { print $mesg . qq(\n); }
@@ -28,21 +30,21 @@ my $base = "ou=San Diego,dc=na,dc=qualcomm,dc=com";
 my $filter = "(|(cn=$username))";
 my $attrs = "cn";
 
-my $result = $ldap->search ( 
+my $result = $ldap->search (
     base    => "$base",
     scope   => "sub",
     filter  => "$filter",
     attrs   =>  $attrs
 );
 
-print qq(Net::LDAP query for user $username returned ) 
+print qq(Net::LDAP query for user $username returned )
     . $result->count() . qq( results\n);
 
 foreach my $record ( $result->entries() ) {
     print qq(Attributes for this record are:\n);
     print join(q(, ), $record->attributes()) . qq(\n);
+    # dump the record out
+    #$record->dump();
 }
 
 $ldap->unbind;
-
-
