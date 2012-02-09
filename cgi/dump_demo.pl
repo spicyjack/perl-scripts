d3316a0e89498158b8668912abed96c9fcb45c80
Brian Manning <brian@xaoc.org>
2012-02-08 23:13:08 -0800

    added HTML::Entities to both scripts
    - Fixed syntax_demo.pl by adding a missing JavaScript library, xregex.js; this
      must be used only when using the CDATA syntax for SyntaxHighlighter

--- cgi/dump_demo.pl
+++ cgi/dump_demo.pl
@@ -7,20 +7,25 @@ use strict;
 use warnings;
 use CGI;
 use CGI::Carp qw(fatalsToBrowser);
+use HTML::Entities;
 
 my $q = CGI->new();
 
 print $q->header(), 
-    $q->start_html(-title => q(Syntax highlight demo)) 
+    $q->start_html(-title => q(Script dumping demo)) 
     . qq(\n);
 
 print $q->h1(q(The source code of this CGI script)) . qq(\n);
 # create a filehandle to read in the contents of this file
 open(IN, q(<) . __FILE__);    
+
 my @script_text = <IN>;
 close(IN);
-my $text = join(qq(\n), @script_text);
-print qq(<pre>$text</pre>\n);
+my $text = join(qq(), @script_text);
+
+# Filter for HTML tags here; need to escape them so the browser doesn't
+# try to re-render them
+print q(<pre>) . encode_entities($text) . qq(</pre>\n);
 print $q->end_html();
 
 exit 0;
--- cgi/syntax_demo.pl
+++ cgi/syntax_demo.pl
@@ -7,6 +7,7 @@ use strict;
 use warnings;
 use CGI;
 use CGI::Carp qw(fatalsToBrowser);
+use HTML::Entities;
 
 my $q = CGI->new();
 
@@ -14,20 +15,27 @@ print $q->header(),
     $q->start_html(-title => q(Syntax highlight demo)) 
     . qq(\n);
 
-print <<EOS;
+print <<EOJS;
 <!-- Include required JS files -->
-<script type="text/javascript" src="js/shCore.js"></script>
-<link href="css/shCore.css" rel="stylesheet" type="text/css" />
+<!-- http://xregexp.com/ -->
+<script type="text/javascript" src="/js/xregexp-min.js"></script>
+<script type="text/javascript" src="/js/shCore.js"></script>
+<link href="/css/shCore.css" rel="stylesheet" type="text/css" />
  
 <!--
-    At least one brush, here we choose JS. You need to include a brush for every 
-    language you want to highlight
+    At least one brush, here we choose JS. You need to include a brush for
+    every language you want to highlight
 -->
-<script type="text/javascript" src="css/shBrushJScript.js"></script>
- 
+<script type="text/javascript" src="/js/shBrushJScript.js"></script>
+<script type="text/javascript" src="/js/shBrushPerl.js"></script>
+
 <!-- Include *at least* the core style and default theme -->
-<link href="css/shThemeDefault.css" rel="stylesheet" type="text/css" />
-EOS
+<link href="/css/shThemeDefault.css" rel="stylesheet" type="text/css" />
+<link href="/css/shCoreDefault.css" rel="stylesheet" type="text/css" /> 
+
+<!-- Tickle the SyntaxHighlighter object -->
+<script type="text/javascript">SyntaxHighlighter.all()</script>
+EOJS
 
 print $q->h1(q(The source code of this CGI script, with syntax highlighting))
     . qq(\n);
@@ -35,12 +43,12 @@ print $q->h1(q(The source code of this CGI script, with syntax highlighting))
 open(IN, q(<) . __FILE__);    
 my @script_text = <IN>;
 close(IN);
-my $text = join(qq(\n), @script_text);
+my $text = join(qq(), @script_text);
 print q(<script type="syntaxhighlighter" class="brush: perl"><![CDATA[) 
     . qq(\n);
-print $text . qq(\n);
+print encode_entities($text) . qq(\n);
 print qq(]]></script>\n);
-print qq|<script type="text/javascript">\nSyntaxHighlighter.all()\n</script>\n|;
+
 print $q->end_html();
 
 exit 0;
