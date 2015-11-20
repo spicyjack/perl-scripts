0563875991ac2ab2ab4f93b0f338ed8e6627a56d
Brian Manning <brian@xaoc.org>
2015-11-20 09:20:50 -0800

    mojo_post_demo.pl: added saving files to HTTP POST demo

--- mojolicious/mojo_post_demo.pl
+++ mojolicious/mojo_post_demo.pl
@@ -7,7 +7,7 @@ use Mojo::Util qw(dumper);
 # - Put your content in a file called 'data.txt'
 # - Call `cURL` with
 #
-# curl -i -X POST -F name=@data.txt \
+# curl -i -X POST -F name=@large_image.jpg \
 # http://localhost:3000/images/upload/54321/528d3604356050ce3ee5776aca7a9098;
 # echo
 
@@ -23,11 +23,16 @@ post q(/images/upload/:ro_number/:checksum) => sub {
   my $c = shift;
   my $ro_number = $c->stash(q(ro_number));
   my $checksum = $c->stash(q(checksum));
-  #my $upload = $c->req->upload($checksum);
+
   #my $upload = Mojo::Upload->new();
   my $log = Mojo::Log->new();
-  #$log->debug(q(Upload filename is: ) . $upload->filename);
-  $log->debug(dumper($c));
+
+  $log->debug(dumper($c->req->content));
+  my $upload = $c->req->upload($checksum);
+  $log->debug(q(Upload filename is: ) . $upload->filename);
+  my $filename = $upload->filename;
+  $log->debug(qq(Moving file $filename to /tmp));
+  $upload->move_to(qq(/tmp/$ro_number.$filename));
   $c->render(
     status => 201,
     text => qq({ "upload_successful": "$ro_number/$checksum" })
